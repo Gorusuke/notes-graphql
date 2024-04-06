510900b5d9de68690bf46b38a899565bbe798f08
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-04-05 19:09:40 -0500

    Add drizzle schemas and database

---
+++ src/drizzle/notesSchema.ts
@@ -0,0 +1,9 @@
+import { text, sqliteTable, integer } from "drizzle-orm/sqlite-core";
+import { categories } from "./categoriesSchema.ts";
+
+export const notes = sqliteTable("notes", {
+  id: integer("id", { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  categoryId: integer("category_id", { mode: 'number' }).references(() => categories.id).default(2)
+});
