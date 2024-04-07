2d05cffa770255ab290823c22b3ac0109177050e
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-04-07 16:40:24 -0500

    Add query and mutation to index file

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,29 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { db } from "./db.ts";
-import { notes } from "./drizzle/notesSchema.ts";
+import { getAllNotes, getNoteById, getNotesByCategoryId, getNotesCount } from "./query/noteQuery.ts";
+import { noteDefinitions } from "./definitions/noteDefinitions.ts";
+import { addNote, deleteNotes, updateArchiveNote, updateNotes, updateNotesByCategoryId, updateUnarchiveNote } from "./mutations/noteMutation.ts";
 
 const typeDefs = `#graphql
-  type Note {
-    id: Int
-    title: String
-    description: String
-    categoryId: String
-  }
-
-  type Query {
-    allNotes: [Note]
-    allNotesCount: Int
-  }
+  ${noteDefinitions}
 `
 
 const resolvers = {
   Query: {
-    allNotes: async () => {
-      const allNotes = await db.select().from(notes).all()
-      return allNotes
-    },
-    allNotesCount: async () => {
-      const allNotes = await db.select().from(notes).all()
-      return allNotes.length
-    }
+    getAllNotes,
+    getNotesCount,
+    getNoteById,
+    getNotesByCategoryId
+  },
+  Mutation: {
+    addNote,
+    updateNotes,
+    updateNotesByCategoryId,
+    updateArchiveNote,
+    updateUnarchiveNote,
+    deleteNotes
   }
 }
-
 
 const server = new ApolloServer({
   typeDefs,
